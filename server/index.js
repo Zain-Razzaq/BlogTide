b1ae9c0356b99e89c4bb02f857808f24a216f270
Zain Razzaq <zainrazzaq2003@gmail.com>
2024-01-16 19:58:18 +0500

    Get root url for server

--- server/index.js
+++ server/index.js
@@ -40,3 +40,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
+
+app.get("/", (req,res) => {
+  res.send("Server is running");
+})
